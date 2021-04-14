ae4cc9a26f3386717d9673033f02ab4f14eece70
Maura Joglekar <m.joglekar@f5.com>
2021-04-14 16:44:05 -0700

    fix devtools with saga

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import counterReducer from "./redux/counter/counterReducer";
 import watchCounter from "./redux/counter/counterSagas";
 import createSagaMiddleware from "redux-saga";
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     counterReducer, /* preloadedState, */
-    applyMiddleware(sagaMiddleware)
+    composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 sagaMiddleware.run(watchCounter);
 
