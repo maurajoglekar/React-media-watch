7b417f1a4c7907e361288650372ee971a3293ca5
Maura Joglekar <m.joglekar@f5.com>
2021-04-14 12:12:30 -0700

    cleaned up initial app and created counter with redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import store from "./store";
+import Counter from "./counter/container/Counter";
+import "./styles.css";
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById("root"));
